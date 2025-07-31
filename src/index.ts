768fab8b423396fc80ec81197226b8787272bc84
Luis Isaac Cuautle Delgadillo <cuautle.tron@gmail.com>
2025-07-31 10:36:39 -0600

    obtiendo la DB de mongo

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import colors from 'colors';
 import server from './server'
 
 const port = process.env.PORT || 4000;
 server.listen( port, () => {
-    console.log(`\nREST API Funcionando en el puerto ${port}\n`);
+    console.log(colors.cyan.bold(`\nREST API Funcionando en el puerto ${port}\n`));
 })
